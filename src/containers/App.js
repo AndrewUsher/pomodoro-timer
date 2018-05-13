cf3e21525afaef8a32e76afe9c90379a30bd78bf
Andrew Usher <andrewusher00@gmail.com>
2018-05-13 09:26:36 -0500

    Add App container component

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Header from '../components/Header'
+import Timer from './Timer'
+import Footer from '../components/Footer'
+
+const App = () => (
+  <Fragment>
+    <Header />
+    <Timer />
+    <Footer />
+  </Fragment>
+)
+export default App
