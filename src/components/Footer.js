f44678ba9798539731c392781d3a5a476a3260c7
Andrew Usher <andrewusher00@gmail.com>
2018-05-13 09:16:30 -0500

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/_footer.styl'
+
+export default () => (
+  <footer>
+    <p>Made with love and code by <a href="http://andrewusher.co">Andrew Usher</a></p>
+  </footer>
+)
